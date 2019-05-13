bd2dd4521736aef5fef30843bf521298db9418fd
Youri Haenen <y.h.b.haenen@student.tue.nl>
2019-05-13 14:18:08 +0200

    Added authentication for testing on every route

--- app/Http/Controllers/SpaController.php
+++ app/Http/Controllers/SpaController.php
@@ -6,6 +6,14 @@ use Illuminate\Http\Request;
 
 class SpaController extends Controller
 {
+    /**
+     * SpaController constructor. Requires auth for all routes for development
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('spa');
