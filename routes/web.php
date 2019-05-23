bbcd7c3d2c88f511cf29a4e176eadb6b787756df
Youri Haenen <y.h.b.haenen@student.tue.nl>
2019-05-23 13:03:40 +0200

    Removed register options

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::resource('/api/stocks', 'StockController');
 Route::resource('/api/orders', 'OrderController');
