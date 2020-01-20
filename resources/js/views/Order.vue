55a28127ced6ea949c13f492fef1f88a5b9ddf77
Youri <y.h.b.haenen@student.tue.nl>
2020-01-20 21:42:54 +0100

    Refactor update in Ordercontroller, added edit order view

--- resources/js/views/Order.vue
+++ resources/js/views/Order.vue
@@ -18,7 +18,7 @@
           <td class="align--top">{{item.stock.item.name}}</td>
           <td class="align--top">{{item.stock.item.gender}}</td>
           <td class="align--top">
-            <select v-model="item.stock.size">
+            <select v-model="item.stock.size" @change="setNewSize($event, item)">
               <option
                 v-for="size in getStock(item.stock.item.slug).stock"
                 :key="size.id"
@@ -50,7 +50,8 @@ export default {
   data: function() {
     return {
       order: {},
-      interval: null
+      interval: null,
+      changedItems: []
     };
   },
   computed: {
@@ -62,7 +63,38 @@ export default {
     goBack() {
       this.$router.go(-1);
     },
-    saveOrder() {}
+    setNewSize(event, item) {
+      let stocks = this.getStock(item.stock.item.slug).stock;
+      let size = stocks.find(size => {
+        return size.size === event.target.value;
+      });
+      let changedItem = {
+        stock_id: size.id,
+        order_id: this.order.id,
+        amount: item.amount,
+        is_picked_up: item.is_picked_up,
+        old_stock: item.stock.id
+      };
+      this.changedItems.push(changedItem);
+    },
+    saveOrder() {
+      axios
+        .patch("/api/orders/" + this.order.id, {
+          data: JSON.stringify(this.changedItems)
+        })
+        .then(result => {
+          let that = this;
+          this.$store.dispatch("fetchOrder", this.$route.params.id);
+          this.data = {};
+
+          setTimeout(function() {
+            that.order = that.$store.getters.getOrder;
+          }, 2000);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   },
   mounted() {
     this.$store.dispatch("fetchItemsDashboard");
