d58c7566cf916c3d2b9dfe22a91f633c5babe700
Youri <y.h.b.haenen@student.tue.nl>
2020-02-01 17:36:23 +0100

    User added to DB if login, password changes also

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,14 +7,36 @@ use App\User;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Psr\Http\Message\ResponseInterface;
+use PhpParser\Node\Expr\Cast\Bool_;
 use stdClass;
 
 class AuthController extends Controller
 {
+
+    private function getFLUser(Request $request, $needs_attributes)
+    {
+        $client = new Client(['base_uri' => 'http://10.3.3.11:5000/']);
+        $post_body = new stdClass;
+        $post_body->key = env('FOOTLOOSE_AUTH_KEY');
+        $post_body->username = $request->username;
+        $post_body->password = $request->password;
+        if ($request->code) {
+            $post_body->otp = $request->code;
+        }
+
+        if ($needs_attributes) {
+            return json_decode($client->request('GET', 'user/info', [
+                'query' => (array) $post_body,
+            ])->getBody()->getContents());
+        } else {
+            return json_decode($client->request('POST', 'login', [
+                'json' => $post_body,
+            ])->getBody()->getContents());
+        }
+    }
+
     public function login(Request $request)
     {
-
         $user = User::where('email', $request->email)->first();
 
         if ($user) {
@@ -27,31 +49,30 @@ class AuthController extends Controller
                 ];
                 return response()->json($response, 200);
             } else {
+                $fl_user = $this->getFLUser($request, false);
+
+                if ($fl_user) {
+                    $user->password = Hash::make($request->password);
+                }
+
                 return response()->json([
                     'message' => 'Wrong username or password',
                     'status' => 422,
                 ]);
             }
         } else {
-            $client = new Client(['base_uri' => 'http://10.3.3.11:5000/']);
-            $post_body = new stdClass;
-            $post_body->key = env('FOOTLOOSE_AUTH_KEY');
-            $post_body->username = $request->username;
-            $post_body->password = $request->password;
-            if ($request->code) {
-                $post_body->otp = $request->code;
-            }
 
-            $user = json_decode($client->request('POST', 'login', [
-                'json' => $post_body,
-            ])->getBody()->getContents());
+            $fl_user = $this->getFLUser($request, true);
 
-            if ($user) {
-                $temp_user = new User([
-                    'email' => $user->username.'@esdvfootloose.nl',
-                    'name' => $user->username
+            if ($fl_user) {
+                $user = new User([
+                    'email' => $fl_user->email,
+                    'name' => $fl_user->first_name . " " . $fl_user->last_name,
+                    'password' => Hash::make($request->password),
                 ]);
-                $token = $temp_user->createToken('Laravel Password Grant Client')->accessToken;
+                $user->save();
+
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
                 $response = [
                     'user' => $user,
                     'token' => $token,
