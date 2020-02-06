12827dae0e068d0a1a0a8dfba9ff4cf32ab958d7
Youri <y.h.b.haenen@student.tue.nl>
2020-02-06 19:57:49 +0100

    Fixed broken link in paid mail

--- resources/views/mail/created.blade.php
+++ resources/views/mail/created.blade.php
@@ -6,7 +6,7 @@ Hi {{ $name }}
 Your Footloose merchandise order has been received properly. You have ordered the following:
 {{ $items }}
 
-You can review your order here, as well as find the payment [link]({!! $order_link !!}). After payment you'll receive a mail confirming this.
+You can review your order here, as well as find the payment [link]({!! url($order_link) !!}). After payment you'll receive a mail confirming this.
 {!! url($order_link) !!}
 
 
